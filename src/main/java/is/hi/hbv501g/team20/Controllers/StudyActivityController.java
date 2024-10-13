27e7d2ac877571c3c8f7ba6b05d41803c4660d33
Luiza Ramos <luiza.s.ramos@gmail.com>
2024-10-13 16:14:45 +0000

    Squashed commit of the following:
    commit 4457b19555935b1c5f6f9b31e10fbe981669a471
    Author: steinunn01 <steinunnm01@gmail.com>
    Date:   Sun Oct 13 11:37:13 2024 +0000
    
        fix date format for studyactivity
    
    commit 2023dccf29bc74450dfcf1472e8838998b429401
    Author: steinunn01 <steinunnm01@gmail.com>
    Date:   Sun Oct 13 11:18:18 2024 +0000
    
        create a studyactivity
    
    commit 2c44cb802c608ccfb0ebb1761e43416fa26fe619
    Author: steinunn01 <steinunnm01@gmail.com>
    Date:   Sat Oct 12 19:07:56 2024 +0000
    
        fix login
    
    commit 7bc3b04368ed6d521cb42970f2e1b9d3525e30d0
    Merge: 82503b3 653e143
    Author: Catarina (Cat) Lima <123655795+CatMLima@users.noreply.github.com>
    Date:   Thu Oct 10 13:07:15 2024 +0000
    
        Merge pull request #12 from CatMLima/pictureUpdating
    
        user picture saves, shows and can be updated in the same page. button…
    
    commit 653e1435d29e30b5e8431a4cf1b607d1391ae22f
    Author: Catarina (Cat) Lima <catisousalima1999@gmail.com>
    Date:   Thu Oct 10 13:06:20 2024 +0000
    
        user picture saves, shows and can be updated in the same page. button to return to the feed when in user profile and also button added to move to user settings
    
    commit 82503b376f7bc62c937878a60df8ae3f8ec7bc2f
    Merge: 4d621f8 ab67016
    Author: spita23 <124268240+spita23@users.noreply.github.com>
    Date:   Wed Oct 9 21:52:05 2024 +0000
    
        Merge pull request #11 from CatMLima/Brynjar
    
        Brynjar
    
    commit ab67016cb690f9af7e9e8362b3b1a63ad4f09ab0
    Author: brynjar <brynjar23@live.com>
    Date:   Wed Oct 9 21:49:10 2024 +0000
    
        When you log in you are now redirected to the feed.
    
    commit ab4f914c79480c2581c77b7514784fc07fc74035
    Author: brynjar <brynjar23@live.com>
    Date:   Wed Oct 9 21:42:03 2024 +0000
    
        Created feed.html page and added controller methods to StudyActivityController
    
    commit 4d621f86f89cdc229a9aad7f97f99aa2acc58fdd
    Merge: e03637e 67ce2f6
    Author: Luiza Ramos <luiza.s.ramos@gmail.com>
    Date:   Sun Oct 6 19:29:14 2024 +0000
    
        Merge pull request #10 from CatMLima/Luiza
    
        Settings

--- src/main/java/is/hi/hbv501g/team20/Controllers/StudyActivityController.java
+++ src/main/java/is/hi/hbv501g/team20/Controllers/StudyActivityController.java
@@ -1,35 +1,85 @@
 package is.hi.hbv501g.team20.Controllers;
 
 import is.hi.hbv501g.team20.Persistence.Entities.StudyActivity;
+import is.hi.hbv501g.team20.Persistence.Entities.User;
+import is.hi.hbv501g.team20.Services.LoginService;
 import is.hi.hbv501g.team20.Services.StudyActivityService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class StudyActivityController {
     private StudyActivityService studyActivityService;
+    private LoginService loginService;
 
     @Autowired
-    public StudyActivityController(StudyActivityService studyActivityService) {
+    public StudyActivityController(StudyActivityService studyActivityService, LoginService loginService) {
         this.studyActivityService = studyActivityService;
+        this.loginService = loginService;
     }
 
-    @RequestMapping(value = "/getsa", method = RequestMethod.GET)
-    public String createActivityGet(StudyActivity studyActivity){
-        return "newStudyActivity";
+    @RequestMapping(value = "/studyactivity-create", method = RequestMethod.GET)
+    public String createStudyActivityGet(Model model) {
+        model.addAttribute("studyactivity", new StudyActivity());
+        return "studyactivity-create";
     }
-    @RequestMapping(value = "/postsa", method = RequestMethod.POST)
-    public String createActivityPOST(StudyActivity studyActivity, BindingResult result, Model model){
-        // If the information given was faulty, return to newItem page to try again.
+
+    @RequestMapping(value = "/api/studyactivity-create", method = RequestMethod.POST)
+    public String createStudyActivity(HttpSession httpSession, StudyActivity studyActivity, BindingResult result, Model model){
+
+        User user = (User) httpSession.getAttribute("user");
+        studyActivity.setUser(user);
+        studyActivity.setDate(new Date());
+        studyActivity.setStart(LocalTime.now());
+        studyActivity.setEnd(LocalTime.now().plusHours(1));
+
         if(result.hasErrors()){
-            return "newStudyActivity";
+            return "studyactivity-create";
         }
-        // Otherwise, save the new item and return to the front page.
         studyActivityService.save(studyActivity);
-        return "redirect:/home";
+        return "redirect:/feed";
     }
+
+    @RequestMapping(value="/studyactivity-details/{ID}", method= RequestMethod.GET)
+    public String getStudyActivityDetailsPage(@PathVariable("ID") long ID, Model model){
+        model.addAttribute("studyactivity", studyActivityService.findById(ID));
+        return "studyactivity-details";
+    }
+
+    // Feed page stuff is here below
+    // Displays feed page
+    @RequestMapping("/feed")
+    public String showFeed(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        List<StudyActivity> allStudyActivities = studyActivityService.findAll();
+        model.addAttribute("studyactivity", allStudyActivities);
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
+        return "feed";
+    }
+
+    // Displays user profile page
+    @RequestMapping("/profile")
+    public String showProfile() {
+        return "user";
+    }
+
+    // Create a study activity
+    @RequestMapping("/studyactivity-create")
+    public String startStudyActivity() {
+        return "studyactivity-create";
+    }
+
+    //End of feed page stuff
 }
