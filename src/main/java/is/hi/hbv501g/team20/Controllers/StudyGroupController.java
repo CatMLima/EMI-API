a92beb5103d814a7bf83ebe7e80e0eae0cd24f89
steinunn01 <steinunnm01@gmail.com>
2024-11-07 11:41:57 +0000

    fix post

--- src/main/java/is/hi/hbv501g/team20/Controllers/StudyGroupController.java
+++ src/main/java/is/hi/hbv501g/team20/Controllers/StudyGroupController.java
@@ -10,10 +10,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -100,7 +97,7 @@ public class StudyGroupController {
 
 
     @RequestMapping(value = "/api/post-create", method = RequestMethod.POST)
-    public String createPost(@RequestParam ("studyGroupId") long studyGroupId, HttpSession httpSession, Post post, BindingResult result) {
+    public String createPost(@RequestParam("studyGroupId") long studyGroupId, HttpSession httpSession, Post post, BindingResult result) {
         User user = (User) httpSession.getAttribute("user");
         StudyGroup studyGroup = studyGroupService.findById(studyGroupId);
         post.setUser(user);
