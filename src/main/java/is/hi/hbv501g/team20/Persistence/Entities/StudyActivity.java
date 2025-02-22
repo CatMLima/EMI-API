c6afb76745aa223b62812306c8dda8bb7fe215a7
steinunn01 <steinunnm01@gmail.com>
2025-02-22 17:02:51 +0000

    fix formatting again method for toString StudyActivity

--- src/main/java/is/hi/hbv501g/team20/Persistence/Entities/StudyActivity.java
+++ src/main/java/is/hi/hbv501g/team20/Persistence/Entities/StudyActivity.java
@@ -242,7 +242,6 @@ public class StudyActivity {
     public String toString() {
         return "StudyActivity{" +
                 "id=" + id +
-                ", userID=" + user.getId()+
                 ", coffees=" + coffees +
                 ", activityPicture=" + Arrays.toString(activityPicture) +
                 ", building=" + building +
